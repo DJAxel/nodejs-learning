e53a1b1da0999c6df5a837ad3abcab95d8da0f5f
Axel Köhler <axel.kohler@elaad.nl>
2024-01-12 17:27:05 +0100

    Add the rest of section 5

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
+const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-    console.log("This runs on every request");
-    next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/add-product", (req, res, next) => {
-    res.send("<h1>Add product page</h1>");
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-    res.send("<h1>Hello from Express!</h1>");
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
