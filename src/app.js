a372f1c5914ad0274773567b1c52b92147533ac4
Axel Köhler <akohler@infiniot.nl>
2024-01-24 15:34:25 +0100

    Add Pug templating engine (part of section 6)

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const admin = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "src/views/");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", admin.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.use((req, res) => {
+    res.status(404).render("404", { pageTitle: "Page not found" });
 });
 
 app.listen(3000);
